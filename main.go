b872920ce007f83997854707a3205dc4d176e66f
Cesar Gimenes <crg@crg.eti.br>
2017-03-04 10:08:18 -0300

    load file with bufio

--- main.go
+++ main.go
@@ -1,5 +1,56 @@
 package main
 
-func main() {
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
 
+	"github.com/crgimenes/goConfig"
+)
+
+type config struct {
+	FileName string `cfg:"name"`
+}
+
+func main() {
+	cfg := config{}
+
+	goConfig.PrefixEnv = "R86"
+	err := goConfig.Parse(&cfg)
+	if err != nil {
+		println(err.Error())
+		os.Exit(1)
+	}
+
+	f, err := os.Open(cfg.FileName)
+	if err != nil {
+		println(err.Error())
+		os.Exit(1)
+	}
+
+	defer func() {
+		err = f.Close()
+		if err != nil {
+			println(err.Error())
+			os.Exit(1)
+		}
+	}()
+
+	buff := bufio.NewReader(f)
+
+	var c byte
+	for {
+
+		c, err = buff.ReadByte()
+		if err != nil {
+			if err == io.EOF {
+				return
+			}
+			println(err.Error())
+		}
+
+		fmt.Printf("%02x ", c)
+
+	}
 }
