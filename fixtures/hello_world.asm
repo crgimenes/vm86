65ee1e4ca1d8ae733f1e8d7939cea76409a38d78
Cesar Gimenes <crg@crg.eti.br>
2020-09-20 10:09:40 -0300

    update .com example

--- fixtures/hello_world.asm
+++ fixtures/hello_world.asm
@@ -1,15 +1,16 @@
+; nasm -f bin -o hello_world.com hello_world.asm
 org 100h
 section .text
 	mov ah, 40h
 	mov bx, 1
-	mov cx, 11
+	mov cx, 12
 	mov dx, msg
 	int 21h
 	mov al,	1
 	mov ah, 4Ch
 	int 21h
 section .data
-	msg db "hello world"
+	msg db "Hello, world"
 
 
 
